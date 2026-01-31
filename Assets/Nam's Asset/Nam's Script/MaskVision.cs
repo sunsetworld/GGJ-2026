cbcc8bad2663fa90f5d5860d564c189aef961bff
HachHNam <hnh1c22@soton.ac.uk>
2026-01-31 17:59:23 +0000

    MaskMechanic test no1

---
+++ Assets/Nam's Asset/Nam's Script/MaskVision.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class MaskVision : MonoBehaviour
+{
+    //[SerializeField] private Cooldown cooldown;
+    public static bool maskOn;
+    [SerializeField] private float CD;
+    [SerializeField] private float cooldownTime;
+    [SerializeField] private float maxCD;
+    [SerializeField] private bool onCooldown = false;
+    [SerializeField] private Slider slider;
+    private bool ok = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (maskOn == true)
+        {
+            // Mask on Action
+            if (CD > 0)
+            {
+                CD -= Time.deltaTime;
+            }
+        }
+        else
+        {
+            // Mask off Action
+        }
+        if (Input.GetKeyDown(KeyCode.Q) && onCooldown == false)
+        {
+            maskOn = true;
+        }
+        slider.value = CD/maxCD;
+        if (CD <= 0 && ok)
+        {
+            StartCoroutine(MaskCoolDown());
+        }
+    }
+    IEnumerator MaskCoolDown()
+    {
+        ok = false;
+        maskOn = false;
+        onCooldown = true;
+        yield return new WaitForSeconds(cooldownTime);
+        CD = maxCD;
+        maskOn = false;
+        onCooldown = false;
+        ok = true;
+
+    }
+}
